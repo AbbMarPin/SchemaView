74ceb9798780d6852c73d1e7d71f7b2593dd0788
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-11-07 09:08:56 +0100

    Ändrade byttiden till 10 sekunder och komenterade alla console.log

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 function today(){
+   // console.log("today")
     
     return new Date()
 }
 
 function calcDiff(date1,date2){
+   // console.log("calcDiff")
     const diff = (((date1.getTime() - date2.getTime())/1000)/60)/60
     //console.log(diff)
     const h_m_array = diff.toString().split('.')
@@ -12,7 +14,7 @@ function calcDiff(date1,date2){
     //console.log(h_m_array[0].slice(0,1))
     //console.log(parseFloat("0."+h_m_array[1])*60)
     if (parseInt(h_m_array[0]) > 0  || h_m_array[0].slice(0,1) != '-'){
-        console.log('Diff',parseInt(h_m_array[0]) + "h",Math.ceil(((parseFloat("0."+h_m_array[1])*60))) + "m")
+        // console.log('Diff',parseInt(h_m_array[0]) + "h",Math.ceil(((parseFloat("0."+h_m_array[1])*60))) + "m")
         return parseInt(h_m_array[0]) + "h" + " " + Math.ceil((parseFloat("0."+h_m_array[1])*60)) + "m"
     } else {
         return false
@@ -21,28 +23,30 @@ function calcDiff(date1,date2){
      
 }
 function renderNextLessons(lesson, time, div){
+   // console.log("renderNextLessons")
     if (lesson.columns[1] === 'Gem aktivitet'){
         if (lesson.columns[4] != ""){
-            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[2] + " börjar om " + time + " i " + lesson.columns[4]+ '\n'
+            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[2] + " börjar om " + time + " i " + lesson.columns[4]+ '\n\n'
         } else {
-            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[2] + " börjar om " + time + '\n'
+            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[2] + " börjar om " + time + '\n\n'
         }   
     }
     else {
         if (lesson.columns[4] != ""){
-            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[1] + " börjar om " + time + " i " + lesson.columns[4] + '\n'   
+            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[1] + " börjar om " + time + " i " + lesson.columns[4] + '\n\n'   
     
         } else {
-            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[1] + " börjar om " + time + '\n'   
+            div.getElementsByClassName('lessons')[0].innerText += lesson.columns[1] + " börjar om " + time + '\n\n'   
         }
     }
 }
 
 function currentLesson(lesson, startTime, currentDate, endTime, div){
+   // console.log("currentLesson")
     if (startTime <= currentDate && currentDate < endTime){
-        console.log("current är", lesson.columns[1])
-        console.log(div)
-        console.log(div.getElementsByClassName('currentLesson'))
+        // console.log("current är", lesson.columns[1])
+        // console.log(div)
+        // console.log(div.getElementsByClassName('currentLesson'))
         
             if (lesson.columns[4] != ""){
                 div.getElementsByClassName('currentLesson')[0].innerText = "Lektion just nu: " + (lesson.columns[1].includes("Moderna")) === true ? "Moderna Språk" :  lesson.columns[1]  + " i " + lesson.columns[4]
@@ -55,6 +59,7 @@ function currentLesson(lesson, startTime, currentDate, endTime, div){
 }
 
 function nextLesson(lessons, div){
+   // console.log("nextLesson")
     lessons.forEach(element => {
         
         var currentDate = today()
@@ -64,7 +69,7 @@ function nextLesson(lessons, div){
         }
 
     
-        console.log(currentDate.getFullYear()+'-'+currentDate.getMonth()+'-'+ currentDay)
+        // console.log(currentDate.getFullYear()+'-'+currentDate.getMonth()+'-'+ currentDay)
         if (element.startdate === currentDate.getFullYear()+'-'+(currentDate.getMonth()+1)+'-'+ currentDay){
             // console.log(element.columns[1])
             // console.log(element.starttime)
@@ -73,7 +78,7 @@ function nextLesson(lessons, div){
             const startTime = new Date(dateStringStart)
             const endTime = new Date(dateStringEnd)
             const timeDiff = calcDiff(startTime, currentDate)
-            console.log(startTime)
+            // console.log(startTime)
             if (timeDiff != false){
                 renderNextLessons(element, timeDiff, div)         
                 
@@ -90,6 +95,7 @@ function nextLesson(lessons, div){
 }
 
 function clear(){
+   // console.log("clear")
     
     for (let x = 0;x < document.getElementsByClassName('currentLesson').length; x++){
         document.getElementsByClassName('currentLesson')[x].innerText=''
@@ -105,13 +111,14 @@ function clear(){
 
 
 function doEverything(klass, url){
+   // console.log("doEverything")
     const divKlass = document.getElementById(klass)
-    console.log(divKlass.getElementsByClassName('break'))
+    // console.log(divKlass.getElementsByClassName('break'))
     
     fetch(url).then(res => res.json())
     .then(resJson => {
         const lessons = resJson.reservations
-        console.log(lessons)
+        // console.log(lessons)
         nextLesson(lessons, divKlass)
         
         
@@ -119,23 +126,26 @@ function doEverything(klass, url){
 }
 
 window.onload = function(){
+   // console.log("window.onload")
     startTime()
     // doEverything("180s", "https://cloud.timeedit.net/abbindustrigymnasium/web/public1/ri1Y7X3QQQfZY6QfZ5064405y7Y7.json")
     // doEverything("190s", "https://cloud.timeedit.net/abbindustrigymnasium/web/public1/ri627Q5Q750ZQ4Q5Y36Q7Zn6y4Z0.json")
 }
 
 
-let latestMin = 1299
+let latestSec = 1299
 let switchTimeEdit = false
 function startTime() {
+   // console.log("startTime")
 
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
+    var s = today.getSeconds();
     //console.log(m)
-    m = checkTime(m);
+    m = addToMinutes(m);
     
-    if (latestMin != m){
+    if (latestSec != s){
         clear()
         doEverything("180s", "https://cloud.timeedit.net/abbindustrigymnasium/web/public1/ri1Y7X3QQQfZY6QfZ5064405y7Y7.json")
         doEverything("190s", "https://cloud.timeedit.net/abbindustrigymnasium/web/public1/ri627Q5Q750ZQ4Q5Y36Q7Zn6y4Z0.json")
@@ -155,10 +165,11 @@ function startTime() {
     
     
     document.getElementById('time').innerText = h + ":" + m;
-    var t = setTimeout(startTime, 500);
-    latestMin = m
+    var t = setTimeout(startTime, 10000);
+    latestSec = s
 }
-function checkTime(i) {
+function addToMinutes(i) {
+   // console.log("checkTime")
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
